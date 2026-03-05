9700cbc01407e9def360b684f4118d1a386ac658
caroo1214 <carolinastoessel715@gmail.com>
2026-03-05 18:16:00 -0300

    solucione problemas de conexion

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 const PDFDocument = require("pdfkit");
+require('dotenv').config();
 
 const app = express();
 
@@ -22,12 +23,11 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGODB_URI;
 
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("✅ Conectado a MongoDB Atlas"))
-.catch(err => console.error("❌ Error de conexión:", err));
+mongoose.connect(MONGO_URI)
+  .then(() => console.log("✅ Conectado a MongoDB Atlas"))
+  .catch(err => console.error("❌ Error de conexión:", err));
+
+
 
 // Esquema de pedidos
 const pedidoSchema = new mongoose.Schema({
